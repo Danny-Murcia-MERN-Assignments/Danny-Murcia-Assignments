53a9af09eee2f8b8295b3bfecf5b56602b730303
dm2800 <danielmurcia@me.com>
2022-06-13 22:32:08 -0700

    puttingittogether

--- react/propitup/src/components/PersonCard.js
+++ react/propitup/src/components/PersonCard.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
 
+    //Deconstructing PersonCard Object
     const {firstName, lastName, age, hairColor} = props;
+
+    //Deconstructing useState 
+    const [ bday, setBday ] = useState(props.age);
+
 
     return (
         <div>
             <h2>{lastName}, {firstName} </h2>
-            <p>Age: {age}</p>
+            <p>Age: {bday}</p>
             <p>Hair Color: {hairColor} </p>
+            <button onClick={ (event) => setBday(bday + 1)}>Birthday Button for {firstName}</button>
         </div>
     )
 }
