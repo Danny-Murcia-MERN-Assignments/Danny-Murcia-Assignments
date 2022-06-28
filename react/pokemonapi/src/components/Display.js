165dc360fefce8454689f10d1dcf3ff2a66df4ec
dm2800 <danielmurcia@me.com>
2022-06-28 16:37:08 -0700

    axiospokemon

--- react/pokemonapi/src/components/Display.js
+++ react/pokemonapi/src/components/Display.js
@@ -1,23 +1,27 @@
 import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
 const Display = (props) => {
 
     const [pokemon, setPokemon] = useState([]);
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-            .then(res => res.json())
-            .then(res => setPokemon(res.results))
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+            .then((res) => {
+                console.log(res);
+                console.log(res.data.results);
+                setPokemon(res.data.results);
+            })
+            .catch((err)=>console.log(err))
     }, []);
 
     return (
         <div>
             <h1 style ={{color: "green"}}>Displaying All Pokemon</h1> 
-            {pokemon.length>0 && pokemon.map((pokemon, index)=>{
+            { pokemon.length>0 && pokemon.map((pokemon, index)=>{
                 return(
                     <div key={index} style={{listStylePosition: "inside"}}>
-                        
-                        <ul >
+                        <ul>
                             <li>{pokemon.name}</li>
                         </ul>
                     </div>
